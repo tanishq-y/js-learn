28d4a527d3b09d348f381146fb5256f9d91699bd
tanishq-y <168284028+tanishq-y@users.noreply.github.com>
2025-05-20 17:07:18 +0000

    config commit codw

--- codw.js
+++ codw.js
@@ -6,4 +6,10 @@ for(let i = 0; i < arr.length; i++){
     }
     
 }
-console.log(newarr);
+console.log(newarr); 
+//  optimized code for this 
+function filter_list(l) {
+  return l.filter(v => typeof v == "number");
+}
+//here l.filter (is a built in array function used to filer array on a specified condition)
+// v=> is arrow function which means for every element in l condition is checked
